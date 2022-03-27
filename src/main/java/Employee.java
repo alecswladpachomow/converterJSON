19431f0580ddca2e2c6809369f92b7c9714860af
alecswladpachomow <alecswlad@yandex.ru>
2022-03-27 14:46:49 +0300

    text: updated task 1.5.2

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -17,14 +17,23 @@ public class Employee {
         this.age = age;
     }
 
-//    @Override
-//    public String toString() {
-//        return "Employee{" +
-//                "id=" + id +
-//                ", firstName='" + firstName + '\'' +
-//                ", lastName='" + lastName + '\'' +
-//                ", country='" + country + '\'' +
-//                ", age=" + age +
-//                '}';
-//    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,11 +5,18 @@ import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -18,11 +25,59 @@ public class Main {
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
         String json = listToJson(list);
-        try (FileWriter file = new FileWriter("data.json")) {
+        fileName = "data.json";
+        fileWrite(json, fileName);
+        fileName = "data.xml";
+        list = parseXML(fileName);
+        fileName = "data2.json";
+        json = listToJson(list);
+        fileWrite(json, fileName);
+    }
+
+    private static void fileWrite(String json, String fileName) {
+        try (FileWriter file = new FileWriter(fileName)) {
             file.write(json);
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static List<Employee> parseXML(String file) {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder;
+        List<Employee> langList = null;
+        try {
+            builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(file);
+            doc.getDocumentElement().normalize();
+            NodeList nodeList = doc.getElementsByTagName("employee");
+            langList = new ArrayList<>();
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                langList.add(getEmployee(nodeList.item(i)));
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+        return langList;
+    }
+
+    private static Employee getEmployee(Node node) {
+        Employee lang = new Employee();
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element element = (Element) node;
+            lang.setId(Integer.parseInt(getTagValue("id", element)));
+            lang.setFirstName(getTagValue("firstName", element));
+            lang.setLastName(getTagValue("lastName", element));
+            lang.setCountry(getTagValue("country", element));
+            lang.setAge(Integer.parseInt(getTagValue("age", element)));
+        }
+        return lang;
+    }
+
+    private static String getTagValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node node = (Node) nodeList.item(0);
+        return node.getNodeValue();
     }
 
     private static String listToJson(List<Employee> list) {
